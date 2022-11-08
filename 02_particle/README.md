0088eeb39a0980d3532d387d91be00ca15265d21
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2022-11-08 09:59:49 +0100

    now particle example is working for the generation of the invariant mass and collision angle histogram

--- 02_particle/README.md
+++ 02_particle/README.md
@@ -8,14 +8,33 @@ the particles.
 
 ## Features
 
-- [ ] Generate a data sample with $10^4$ events
-- [ ] In each event a B meson decays to a pion and a kaon. In the center
+- [x] Generate a data sample with $10^4$ events
+- [x] In each event a B meson decays to a pion and a kaon. In the center
         of mass of the B meson, the decay products have momentum $p^*$
         and their direction is random.
-- [ ] Boost the momentum of the pion and the kaon to the lab reference frame,
+- [x] Boost the momentum of the pion and the kaon to the lab reference frame,
         in this reference frame compute the invariant mass of pion and kaon
         and the opening angle between the kaon and the pion and fill a histogram
+- [ ] Perform *smearing*
 
 ## Design
 
 ## Compilation and Use
+
+The program can be compiled in the following manner
+
+```console
+$ g++ -o particle particle.cpp `root-config --libs --cflags`
+```
+
+and can be subsequently run as follows
+
+```console
+$ ./particle
+```
+
+This will generate three new files:
+
+- `data/particle.root`, which contains the `TTree` object with the generated data
+- `figures/invariant_mass.pdf`, which contains the histogram of the invariant mass of $\pi$ and $K$
+- `figures/collision_angle.pdf`, which contains the histogram for the opening angle between $\pi$ and $K$
--- 02_particle/particle.cpp
+++ 02_particle/particle.cpp
@@ -1,6 +1,11 @@
 #include <vector>
 #include <iostream>
 
+// Visualization
+#include "TH1F.h"
+#include "TCanvas.h"
+
+// Utilities
 #include "TRandom.h"
 #include "TMath.h"
 #include "TLorentzVector.h"
@@ -23,27 +28,57 @@ int main (int argc, char* argv[]) {
 
     TLorentzVector p4_pi, p4_k;
     double px_com, py_com, pz_com;
+    double invmass, theta;
     double m_pi = 140; // MeV
     double m_k = 500; // MeV
     double pstar = 2607; // MeV
     double beta = 0.0567;
 
+    TH1F hinvmass("hinvmass", "distribution of the invariant mass", 100, 3600, 3900);
+    TH1F htheta("htheta", "distribution of the angle between the momenta", 100, 3., 3.2);
+
     tree->Branch("p4_pi", &p4_pi, "p4_pi/l");
     tree->Branch("p4_k", &p4_k, "p4_k/l");
+    tree->Branch("invmass", &invmass, "invmass/D");
+    tree->Branch("theta", &theta, "theta/D");
 
     TRandom* gen = new TRandom();
     gen->SetSeed(0);
 
-    int nevs=400000;
+    int nevs=10000;
 
     for (int iev=0; iev<nevs; iev++) {
+        // Generate a random momentum vector with fixed magnitude
         gen->Sphere(px_com, py_com, pz_com, pstar);
+        // Set the (+ - - -) signature TLorentzVector with component values
         p4_pi.SetPxPyPzE(px_com, py_com, pz_com, sqrt(m_pi*m_pi + px_com*px_com + py_com*py_com + pz_com*pz_com));
         p4_k.SetPxPyPzE(-px_com, -py_com, -pz_com, sqrt(m_k*m_k + px_com*px_com + py_com*py_com + pz_com*pz_com));
+        // Boost the TLorentzVector on the x axis
         p4_pi.Boost(beta, 0, 0);
         p4_k.Boost(beta, 0, 0);
+        // Compute the invariant mass and the angle between TLorentzVectors
+        invmass = sqrt(p4_pi.M2() + p4_k.M2() + 2.*pstar*pstar);
+        theta = p4_pi.Angle(p4_k.Vect());
+        // Fill the histogram
+        hinvmass.Fill(invmass);
+        htheta.Fill(theta);
+        // Fill the tree
         tree->Fill();
     }
+
+    TCanvas canv("canv", "canvas for plotting", 1280, 1024);
+    hinvmass.GetXaxis()->SetTitle("Invariant mass of the pion and kaon [MeV]");
+    hinvmass.Draw();
+
+    canv.SaveAs("./figures/invariant_mass.pdf");
+
+    htheta.GetXaxis()->SetTitle("Angle between the momenta [rad]");
+    htheta.Draw();
+
+    canv.SaveAs("./figures/collision_angle.pdf");
+
+    hinvmass.Write();
+    htheta.Write();
 
     tree->Write();
     tree->Print();
